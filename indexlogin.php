e2f24f2f2532dafe43ea8533e71d8110692846c3
Aldairtunq <201912945b@utea.edu.pe>
2022-10-19 21:50:24 -0500

    index

---
+++ indexlogin.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>BIEVENIDOS</title>
+    
+  </head>
+  <body>
+   
+      <h1>Por favor ingresa o regístrate</h1>
+
+      <a href="loguearse.php">iniciar sesion</a> or
+      <a href="registrarse.php">Inscribirse</a>
+  </body>
+</html>
