54b3b065a44cc2a637dca4cc5c99fb273fdb24b6
Aldairtunq <201912945b@utea.edu.pe>
2022-10-19 22:09:49 -0500

    para iniciar sesion

---
+++ loguearse.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Login</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+  </head>
+  <body>
+    
+
+    <h1>Login</h1>
+ 
+    <form action="login.php" method="POST">
+      <input name="email" type="text" placeholder="Introduce tu correo electrónico">
+      <input name="password" type="password" placeholder="Ingresa tu contraseña">
+      <input type="submit" value="Send">
+    </form>
+  </body>
+</html>
