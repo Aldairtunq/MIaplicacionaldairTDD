2419176438e64c6d09df70f896afe60456d668fb
Aldairtunq <201912945b@utea.edu.pe>
2022-10-22 01:24:02 -0500

    registrarse

--- conexiologin.php
+++ conexiologin.php
@@ -3,7 +3,7 @@
 $server = 'localhost3306';
 $username = 'root';
 $password = '';
-$database = 'php_login_database';
+$database = 'databasealdair';
 
 try {
     $conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
