4416b6c22e782eb7da5495a938ce00fff7eb2c9b
Aldairtunq <201912945b@utea.edu.pe>
2022-10-21 23:47:41 -0500

    conecxion con BDD

--- registrarse.php
+++ registrarse.php
@@ -1,3 +1,9 @@
+<?php> 
+
+require 'conexiologin.php';
+
+?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -11,8 +17,14 @@
     <?php require 'otroscodigos/header.php' ?>
 
      
-    <h1>SignUp</h1>
+    <h1>Inscribirse</h1>
     <span>or <a href="login.php">Login</a></span>
+    <form action="signup.php" method="POST">
+      <input name="email" type="text" placeholder="Introduce tu correo electrónico">
+      <input name="password" type="password" placeholder="ingrese su contraseña">
+      <input name="confirm_password" type="password" placeholder="Confirmar contraseña">
+      <input type="submit" value="enviar">
+    </form>
 
   </body>
 </html>
