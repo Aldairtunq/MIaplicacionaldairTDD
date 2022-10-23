101943506b293b410dc4a811067f238bc80e9fb2
Aldairtunq <201912945b@utea.edu.pe>
2022-10-22 23:12:27 -0500

    para insertar cod,dni,nombre a la tabla alumno

--- pantallalogin/insertar.php
+++ pantallalogin/insertar.php
@@ -9,4 +9,12 @@ $nombres=$_POST['nombres'];
 $apellidos=$_POST['apellidos'];
 
 
+$sql="INSERT INTO alumno VALUES('$cod_estudiante','$dni','$nombres','$apellidos')";
+$query= mysqli_query($con,$sql);
+
+if($query){
+    Header("Location: alumno.php");
+    
+}else {
+}
 ?>
