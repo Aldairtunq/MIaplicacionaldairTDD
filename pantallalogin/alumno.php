5044fb10a424e58cb72178638cea08cbab6a366a
Aldairtunq <201912945b@utea.edu.pe>
2022-10-22 22:59:51 -0500

    indexalumno

---
+++ pantallalogin/alumno.php
@@ -0,0 +1,54 @@
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title> PAGINA ALUMNO</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+        
+    </head>
+    <body>
+            <div class="container mt-5">
+                    <div class="row"> 
+                        
+                        <div class="col-md-3">
+                            <h1>Ingrese datos</h1>
+                                <form action="insertar.php" method="POST">
+
+                                    <input type="text" class="form-control mb-3" name="cod_estudiante" placeholder="cod estudiante">
+                                    <input type="text" class="form-control mb-3" name="dni" placeholder="Dni">
+                                    <input type="text" class="form-control mb-3" name="nombres" placeholder="Nombres">
+                                    <input type="text" class="form-control mb-3" name="apellidos" placeholder="Apellidos">
+                                    
+                                    <input type="submit" class="btn btn-primary">
+                                </form>
+                        </div>
+
+                        <div class="col-md-8">
+                            <table class="table" >
+                                <thead class="table-success table-striped" >
+                                    <tr>
+                                        <th>Codigo</th>
+                                        <th>Dni</th>
+                                        <th>Nombres</th>
+                                        <th>pellidos</th>
+                                        <th></th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+
+                              
+                            </table>
+                        </div>
+                    </div>  
+            </div>
+    </body>
+</html>
